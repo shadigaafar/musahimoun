a5a87dba1366b2127d0eee7b8e0b9841022f76c9
shadigaafar <shadialaghbari@gmail.com>
2025-09-14 01:03:13 +0200

    Remove unused imports and clean up subscription logic in index.js

--- front/src/index.js
+++ front/src/index.js
@@ -2,8 +2,6 @@ import { registerStore, store, updatePostMeta } from './store/index';
 import domReady from '@wordpress/dom-ready';
 import { subscribe, select } from '@wordpress/data';
 import { isEmpty, map, flatten } from 'lodash';
-import {store as editorStore} from '@wordpress/editor'
-import {store as coreStore} from '@wordpress/core-data'
 
 function getAllContributorIds(roleAssignments) {
 	const contributors = map(roleAssignments, 'contributors');
@@ -12,9 +10,8 @@ function getAllContributorIds(roleAssignments) {
 // Later, if necessary...
 domReady(() => {
 	registerStore();
-	
-	subscribe(() => {
 
+	subscribe(() => {
 		const updateRoleAssignments = select(store)?.getRoleAssignments();
 		const allPostContributorIds = getAllContributorIds(
 			updateRoleAssignments
@@ -22,7 +19,5 @@ domReady(() => {
 		if (!isEmpty(updateRoleAssignments)) {
 			updatePostMeta(updateRoleAssignments, allPostContributorIds);
 		}
-
 	}, store);
-
 });
